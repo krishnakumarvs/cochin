038cae1eecc7f590ef74c7a34f8076e9900ad0a4
krishnakumarvs <krishh_mea@yahoo.in>
2017-12-20 13:52:49 +0530

    Default port changed.

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ http.createServer(function(request, response) {
 		console.log("Requested URL is: " + request.url);
 		response.end();
 	}
-}).listen(80);
+}).listen(4200);
 
 function sendFileContent(response, fileName, contentType){
 	fs.readFile(fileName, function(err, data){
